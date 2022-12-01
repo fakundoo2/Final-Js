5a0300d644f0ca03fb14534f0196992bf1707419
Facundo Vera <98712723+fakundoo2@users.noreply.github.com>
2022-12-01 14:26:42 -0300

    proyecto JS con DOM y localstorage

--- script.js
+++ script.js
@@ -2,29 +2,36 @@ let nuevoNombre;
 let nuevoApellido;
 let nuevoEdad;
 let nuevoDni;
-
+let isActive = true;
 let nuevoPersona;
 
-let baseDatos = [];
+let _empleados = [];
+let _despedidos = [];
 
 const botonModal = document.querySelector("#btn-modal");
 const modal = document.querySelector("#modal");
 const botonAgregar = document.querySelector("#btn-agregar");
-const main = document.getElementById("#main");
+const listaEmpleados = document.querySelector("#lista-empleados");
+let botonDespedido = document.querySelectorAll("#btn-despedir");
+const listaDespedidos = document.querySelector("#lista-despedidos");
 
 botonModal.addEventListener("click",() => {
-    modal.classList.add("active");
+    if(isActive){
+        modal.classList.add("active");
+        isActive = false;
+    }else if(!isActive){
+        modal.classList.remove("active");
+        isActive = true;
+    }
 })
 
 botonAgregar.addEventListener("click",() => {
     modal.classList.remove("active");
-    constructor();
-    console.log(baseDatos);
-    console.log(baseDatos.nombre);
-    console.log(baseDatos.nuevoNombre);
+    listaEmpleados.innerHTML = "";
+    capturarDatos();
 })
 
-function constructor(){
+function capturarDatos(){
 
     function persona(nombre,apellido,edad,dni){
         this.nombre = nombre;
@@ -39,26 +46,72 @@ function constructor(){
     nuevoDni = document.querySelector("#dni").value;
 
     nuevoPersona = new persona(nuevoNombre,nuevoApellido,nuevoEdad,nuevoDni);
-    baseDatos.push(nuevoPersona);
-
-    /*PROBANDO COMO TOMAR LOS DATOS DENTRO DE baseDatos */
-    baseDatos.forEach(sujetos =>{
-        console.log("entro");
-        console.log(baseDatos.nuevoNombre);
-    })
-
+    _empleados.push(nuevoPersona);
+    guardarEnLocal();
+    cargarEmpleados();
+    despedirGente();
 }
 
 
+function despedirGente(){
+    botonDespedido = document.querySelectorAll("#btn-despedir");
+    botonDespedido.forEach(boton => {
+        boton.addEventListener("click",() =>{
+            const findpersonaindex = _empleados.findIndex(em => em.dni == empleado.dni);
+            const find = _empleados.find(em => em.dni == empleado.dni);
+            _despedidos.push(find);
+            _empleados.splice(findpersonaindex,1);
+            console.log(findpersonaindex);
+            console.log(_despedidos);
+            guardarEnLocal();
+            cargarEmpleados();
+            cargarDespedidos();
+        } );
 
-function cargarPersona(){
-        baseDatos.forEach(sujetos =>{
-            let div = document.createElement("div");
-            div.classList.add("personas");
-            div.innerHTML = ` 
 
-
-            ` 
-        })
+    });
+    
 }
 
+function cargarEmpleados(){
+    listaEmpleados.innerHTML = "";
+    for(empleado of _empleados){
+        let div = document.createElement("div");
+        div.classList.add("marco");
+        div.innerHTML +=`
+                <p>Nombre:${empleado.nombre}</p>
+                <p>Apeliido:${empleado.apellido}</p>
+                <p>Edad:${empleado.edad}</p>
+                <p>DNI:${empleado.dni}</p>
+                <button class="btn-despedir" id="btn-despedir">Despedir</button>
+        `
+        listaEmpleados.append(div);
+    }
+
+}
+
+function cargarDespedidos(){
+
+    let despedidos = JSON.parse(localStorage.getItem("despedidos"));
+    _despedidos = despedidos;
+    listaDespedidos.innerHTML = "";
+    for(despedido of _despedidos){
+        let div = document.createElement("div");
+        div.classList.add("marco-rojo");
+        div.innerHTML +=`
+                <p>Nombre:${despedido.nombre}</p>
+                <p>Apeliido:${despedido.apellido}</p>
+                <p>Edad:${despedido.edad}</p>
+                <p>DNI:${despedido.dni}</p>
+        `
+        listaDespedidos.append(div);
+    }
+
+}
+
+function guardarEnLocal(){
+    localStorage.setItem("empleados", JSON.stringify(_empleados));
+    localStorage.setItem("despedidos", JSON.stringify(_despedidos));
+
+}
+cargarDespedidos();
